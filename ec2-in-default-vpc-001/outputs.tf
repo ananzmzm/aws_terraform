08a75afd9de79df69de0df8c451eb3d9cb9a8932
Kyoungduk Yoo <ttog09@gmail.com>
2022-03-10 18:46:17 +0900

    [REMOVED] directory names were modified.

--- ec2-in-default-vpc-001/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value = aws_instance.basic.*.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value = aws_instance.basic.*.public_ip
-}
